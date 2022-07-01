9ef5d38d5f383fadd686daed42c8647114b06255
AdhamSewelam <89882834+AdhamSewelam@users.noreply.github.com>
2022-07-01 23:29:39 +0200

    Update display.js

--- display.js
+++ display.js
@@ -1,30 +1,40 @@
-
 function displayDisease(meal) {
-    fetch(`https://medicalproject1010.herokuapp.com/conditions?searchKey=${meal}`)
-        .then(data => {
-            return data.json();
-        })
-        .then(condition => {
-            const conditions = condition.data
-            var cols = `
-                    <a href="index.html" class="textDiseases">Diseases</a>
-                    <a href="pageDisplay.html" class="textDiseases">> ${conditions.diseaseName}</a>
-                    <h1>${conditions.diseaseName}</h1> <hr>
-                    <h2>Overview</h2>
+  fetch(`https://medicalproject1010.herokuapp.com/conditions?searchKey=${meal}`)
+    .then(data => {
+      return data.json();
+    })
+    .then(condition => {
+      const titleDisease = document.getElementById("titleDisease")
+      const conditions = condition.data
+      var cols = `
+                    <a href="index.html" class="btn btn-outline-primary rounded-pill  mt-5 d-block w-25 textDiseases">Diseases</a>
+                    <a href="pageDisplay.html" class="btn btn-outline-primary rounded-pill mt-2  d-block w-25 textDiseases"> ${conditions.diseaseName}</a>
+                    <h1 class="mt-5 searchColorH1">${conditions.diseaseName}</h1> <hr>
+                    <h2 class="searchColor mt-2">Overview</h2>
                     <p>${conditions.overview}</p>
-                    <h2>Symptoms</h2>
+                    <div align=center>
+                    <img src="${conditions.diseaseImage[0]}" alt="" />
+                    </div>
+                    <h2 class="searchColor my-5"">Symptoms</h2>
                     <p>${conditions.symptoms}</p>
-                    <h2>Causes</h2>
+                    <h2 class="searchColor my-5"">Causes</h2>
                     <p>${conditions.causes}</p>
-                    <h2>Risk factors</h2>
+                    <div align=center>
+                    <img src="${conditions.diseaseImage[1]}" class="img-fluid h-50 w-25" alt="" />
+                    </div>
+                    <h2 class="searchColor my-5"">Risk factors</h2>
                     <p>${conditions.riskFactors}</p>
-                    <h2>Complications</h2>
+                    <h2 class="searchColor my-5"">Complications</h2>
                     <p>${conditions.complications}</p>
-                    <h2>Prevention</h2>
+                    <h2 class="searchColor my-5"">Prevention</h2>
                     <p>${conditions.prevention}</p>
+                    <p>${conditions.causes}</p>
+                    <div align=center>
+                    <img src="${conditions.diseaseImage[2]}" class="img-fluid h-50 w-25" alt="" />
+                    </div>
               `;
-            document.getElementById("display").innerHTML = cols;
-        });
+      document.getElementById("display").innerHTML = cols;
+    });
 }
 
 displayDisease(localStorage.getItem("disease"))
